d8a42fa80d54862b4bf4a7e740d75cf9a518a1f6
leonardobgsilva <139085374+leonardobgsilva@users.noreply.github.com>
2025-03-30 22:30:29 -0300

    Add files via upload

---
+++ user_data/green-app.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y nodejs npm git
+cd /home/ec2-user
+git clone --branch alb-green --single-branch https://github.com/leonardobgsilva/app-leocinema-lite.git 
+sudo chown -R ec2-user:ec2-user /home/ec2-user/app-leocinema-lite
+cd app-leocinema-lite
+sudo npm install
+
+sudo bash -c 'cat <<EOF > /etc/systemd/system/leocinema-lite.service
+[Unit]
+Description=Leo Cinema Lite
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/node server.js
+WorkingDirectory=/home/ec2-user/app-leocinema-lite
+Restart=always
+User=root
+Environment=PATH=/usr/bin:/usr/local/bin
+Environment=NODE_ENV=production
+LimitNOFILE=4096
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+sudo systemctl daemon-reload
+sudo systemctl enable leocinema-lite
+sudo systemctl start leocinema-lite
